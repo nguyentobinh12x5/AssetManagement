cd3df806c4b0c5a62513308bd008cab58e25ef59
nguyentobinh <rookiedev06@toanleh.onmicrosoft.com>
2024-06-17 13:40:41 +0700

    chore: configure applicationUserProfile with automapper

---
+++ src/Infrastructure/Identity/ApplicationUserProfile.cs
@@ -0,0 +1,12 @@
+﻿using AssetManagement.Application.Users.Queries.GetUser;
+using AutoMapper;
+
+namespace AssetManagement.Infrastructure.Identity;
+
+public class ApplicationUserProfile : Profile
+{
+    public ApplicationUserProfile()
+    {
+        CreateMap<ApplicationUser, UserDto>();
+    }
+}
