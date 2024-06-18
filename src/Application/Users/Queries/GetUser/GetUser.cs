f7c0271c67cf50fe20b2b3c92bd77cfd421d9336
nguyentobinh <rookiedev06@toanleh.onmicrosoft.com>
2024-06-18 13:00:15 +0700

    feature (api): add get user by id

---
+++ src/Application/Users/Queries/GetUser/GetUser.cs
@@ -0,0 +1,27 @@
+﻿using AssetManagement.Application.Common.Interfaces;
+
+namespace AssetManagement.Application.Users.Queries.GetUser;
+
+public record GetUserQuery() : IRequest<UserDto>
+{
+    public string Id { get; set; } = null!;
+};
+
+public class GetUsersQueryHandler : IRequestHandler<GetUserQuery, UserDto>
+{
+    private readonly IIdentityService _identityService;
+
+    public GetUsersQueryHandler(
+        IIdentityService identityService
+    )
+    {
+        _identityService = identityService;
+    }
+
+    public async Task<UserDto> Handle(GetUserQuery request, CancellationToken cancellationToken)
+    {
+        var user = await _identityService.GetUserWithRoleAsync(request.Id);
+        
+        return user;
+    }
+}
