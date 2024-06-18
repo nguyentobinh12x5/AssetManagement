ba2e35a2b8cb5ec2a68ff87a5943b5ee5dfe4937
Kien Nguyen <rookiedev02@toanleh.onmicrosoft.com>
2024-06-18 16:14:13 +0700

    feat: Change password on firstime login

--- src/Web/client-app/src/features/auth/firstime-login/useFirstTimeLogin.ts
+++ src/Web/client-app/src/features/auth/firstime-login/useFirstTimeLogin.ts
@@ -1,25 +1,26 @@
-import { FormikHelpers } from "formik";
-import * as Yup from "yup";
-import { useAppDispatch } from "../../../redux/redux-hooks";
+import { FormikHelpers } from 'formik';
+import * as Yup from 'yup';
+import { useAppDispatch } from '../../../redux/redux-hooks';
 
 const ChangePasswordFirstimeSchema = Yup.object().shape({
   newPassword: Yup.string().required('Required'),
 });
-export interface IChangePasswordFirstTime extends Yup.InferType<typeof ChangePasswordFirstimeSchema> {}
+export interface IChangePasswordFirstTime
+  extends Yup.InferType<typeof ChangePasswordFirstimeSchema> {}
 
 const useFirstTimeLogin = () => {
-    const dispatch = useAppDispatch();
-    const initialValues : IChangePasswordFirstTime = {newPassword: ""}
+  const dispatch = useAppDispatch();
+  const initialValues: IChangePasswordFirstTime = { newPassword: '' };
 
-    const handleSubmit = (
-        values: IChangePasswordFirstTime,
-        actions: FormikHelpers<IChangePasswordFirstTime>
-      ) => {
-        // dispatch(login(values));
-        actions.setSubmitting(false);
-      };
+  const handleSubmit = (
+    values: IChangePasswordFirstTime,
+    actions: FormikHelpers<IChangePasswordFirstTime>
+  ) => {
+    // dispatch(login(values));
+    actions.setSubmitting(false);
+  };
 
-    return {initialValues, handleSubmit};
-}
+  return { initialValues, handleSubmit };
+};
 
 export default useFirstTimeLogin;
