86fce679afbdad75bdf6e505d94b2bb29f7e6409
Phuong Nguyen <rookiedev03@toanleh.onmicrosoft.com>
2024-06-19 14:50:27 +0700

    feat(common): add extension password auto generated

---
+++ src/Application/Common/Extensions/PasswordGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetManagement.Application.Common.Extensions
+{
+    public static class PasswordGenerator
+    {
+        public static string GeneratePassword(this string username, DateTime dateOfBirth)
+        {
+            return $"{char.ToUpper(username[0])}{username[1..]}@{dateOfBirth:ddMMyyyy}";
+        }
+    }
+}
