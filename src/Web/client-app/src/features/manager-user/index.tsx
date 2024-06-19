408c00f884f81455eedaabc4ac31533853a2c5f1
Phuong Nguyen <rookiedev03@toanleh.onmicrosoft.com>
2024-06-20 02:25:29 +0700

    feat: add public route user

--- src/Web/client-app/src/features/manager-user/index.tsx
+++ src/Web/client-app/src/features/manager-user/index.tsx
@@ -1,13 +1,15 @@
 ﻿import React, { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
-import { EDIT_USER } from "./constants/edit-user";
+import { CREEATE_USER, EDIT_USER } from "./constants/edit-user";
+import CreateUser from "./create";
 
 const EditUser = lazy(() => import("./edit"));
 
 const Users = () => {
   return (
     <Routes>
-      <Route path={EDIT_USER} element={<EditUser />} />
+          <Route path={EDIT_USER} element={<EditUser />} />
+          <Route path={CREEATE_USER} element={<CreateUser />} />
     </Routes>
   );
 };
