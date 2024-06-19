f840ea27ab7f868589f03c98aed4f35fb8c66620
Phuong Nguyen <rookiedev03@toanleh.onmicrosoft.com>
2024-06-19 14:53:12 +0700

    feat(user): add config mapping UserDTOs

--- src/Infrastructure/Identity/ApplicationUserProfile.cs
+++ src/Infrastructure/Identity/ApplicationUserProfile.cs
@@ -1,5 +1,5 @@
-﻿using AssetManagement.Application.Users.Queries.GetUser;
-using AssetManagement.Domain.Enums;
+﻿using AssetManagement.Application.Users.Commands.Create;
+using AssetManagement.Application.Users.Queries.GetUser;
 using AutoMapper;
 
 namespace AssetManagement.Infrastructure.Identity;
@@ -9,5 +9,6 @@ public class ApplicationUserProfile : Profile
     public ApplicationUserProfile()
     {
         CreateMap<ApplicationUser, UserDto>().ReverseMap(); ;
+        CreateMap<ApplicationUser, UserDTOs>();
     }
 }
