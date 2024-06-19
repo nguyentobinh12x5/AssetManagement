011af890feae6b903132d9f8e2741d0137fd83a6
Phuong Nguyen <rookiedev03@toanleh.onmicrosoft.com>
2024-06-19 14:51:22 +0700

    feat(common): add extension username auto generated

---
+++ src/Application/Common/Extensions/UsernameGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetManagement.Application.Common.Extensions
+{
+    public static class UsernameGenerator
+    {
+        public static string GenerateUsername(this IEnumerable<string?> existingUsernames, string firstName, string lastName)
+        {
+            string FirstName = firstName.ToLowerInvariant();
+
+            string[] lastNameParts = lastName.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string LastName = string.Concat(lastNameParts.Select(part => part[..1].ToLowerInvariant()));
+
+
+            string baseUsername = $"{firstName}{LastName}";
+
+            int suffix = 0;
+
+            string newUsername = baseUsername;
+
+            while (existingUsernames.Contains(newUsername))
+            {
+                suffix++;
+                newUsername = $"{baseUsername}{suffix}";
+            }
+
+            return newUsername;
+        }
+    }
+}
