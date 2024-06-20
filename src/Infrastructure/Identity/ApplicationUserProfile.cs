74d43110e96238594a58483ead5fe1a5c607b0e7
LmTrieu <lmtrieu250902@gmail.com>
2024-06-20 13:40:21 +0700

    fix(Mapper): Add option for mapping FullName

--- src/Infrastructure/Identity/ApplicationUserProfile.cs
+++ src/Infrastructure/Identity/ApplicationUserProfile.cs
@@ -8,6 +8,12 @@ public class ApplicationUserProfile : Profile
 {
     public ApplicationUserProfile()
     {
+        CreateMap<ApplicationUser, UserDto>().ReverseMap(); ;
+        CreateMap<ApplicationUser, UserBriefDto>()
+            .ForMember(
+                dest => dest.FullName,
+                opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}")
+            );
         CreateMap<ApplicationUser, UserBriefDto>();
         CreateMap<UserInfoDto, ApplicationUser>();
     }
