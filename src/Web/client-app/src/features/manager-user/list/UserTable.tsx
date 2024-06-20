afcfbb4a4eaff273fc4ecd50c03f9c2511ae1779
Phuong Nguyen <rookiedev03@toanleh.onmicrosoft.com>
2024-06-20 13:35:33 +0700

    feat: add view detail user

--- src/Web/client-app/src/features/manager-user/list/UserTable.tsx
+++ src/Web/client-app/src/features/manager-user/list/UserTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import IColumnOption from "../../../components/table/interfaces/IColumnOption";
 import IPagination from "../../../components/table/interfaces/IPagination";
 import ISortState from "../../../components/table/interfaces/ISortState";
@@ -8,6 +8,9 @@ import Table from "../../../components/table/Table";
 import ButtonIcon from "../../../components/ButtonIcon";
 import { useNavigate } from "react-router-dom";
 import { PencilFill } from "react-bootstrap-icons";
+import PopupComponent from "../details";
+import { Button } from "../../../components";
+import Loading from "../../../components/Loading";
 
 type UserTableProps = {
   users: IPagedModel<IBriefUser>;
@@ -37,38 +40,76 @@ const UserTable: React.FC<UserTableProps> = ({
   const handleEditClick = (userId: string) => {
     navigate(`edit/${userId}`);
   };
+  const handleCreateUser = () => {
+    navigate(`create`);
+  }
+  const [selectedUser, setSelectedUser] = useState<string | null>(null);
+  const [showPopup, setShowPopup] = useState(false);
 
+  const handleShowPopup = (userId: string) => {
+    setSelectedUser(userId);
+    setShowPopup(true);
+  };
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+    setSelectedUser(null);
+  };
+  const formatDate = (date: Date) => {
+    const parsedDate = typeof date === 'string' ? new Date(date) : date;
+    const day = String(parsedDate.getDate()).padStart(2, '0');
+    const month = String(parsedDate.getMonth() + 1).padStart(2, '0');
+    const year = parsedDate.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
   const pagination: IPagination = {
     currentPage: pageNumber,
     totalPage: totalPages,
     handleChange: handlePaging,
   };
-
+  if (!users) {
+    return (
+       <Loading />
+    );
+  }
   return (
-    <Table
-      columns={columns}
-      sortState={sortState}
-      handleSort={handleSort}
-      pagination={pagination}
-    >
-      {items?.map((data) => (
-        <tr key={data.id}>
-          <td>{data.staffCode}</td>
-          <td>{data.fullName}</td>
-          <td>{data.userName}</td>
-          <td>{data.joinDate.toString()}</td>
-          <td>{data.type}</td>
-          <td>
-            <ButtonIcon
-              onClick={() => handleEditClick(data.id)}
-              disable={false}
-            >
-              <PencilFill></PencilFill>
-            </ButtonIcon>
-          </td>
-        </tr>
-      ))}
-    </Table>
+    <>
+      <Button onClick={() => handleCreateUser()}>Create new user</Button>
+      <Table
+        columns={columns}
+        sortState={sortState}
+        handleSort={handleSort}
+        pagination={pagination}
+      >
+        {items?.map((data) => (
+          <tr key={data.id} onClick={() => handleShowPopup(data.id)} style={{ cursor: 'pointer' }}>
+            <td>{data.staffCode}</td>
+            <td>{data.fullName}</td>
+            <td>{data.userName}</td>
+            <td>{formatDate(data.joinDate)}</td>
+            <td>{data.type}</td>
+            <td>
+              <ButtonIcon
+                onClick={() => {
+                   
+                  handleEditClick(data.id);
+                }}
+                disable={false}
+              >
+                <PencilFill></PencilFill>
+              </ButtonIcon>
+            </td>
+          </tr>
+        ))}
+      </Table>
+      {selectedUser && (
+        <PopupComponent
+          show={showPopup}
+          handleClose={handleClosePopup}
+          userId={selectedUser}
+        />
+      )}
+    </>
   );
 };
 
