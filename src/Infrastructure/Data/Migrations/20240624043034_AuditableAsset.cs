ba60b1bde5ebabaf03ca165ccde904a2bc1e1f0e
nguyentobinh <rookiedev06@toanleh.onmicrosoft.com>
2024-06-24 11:32:32 +0700

    feat: add auditable to asset and category model

---
+++ src/Infrastructure/Data/Migrations/20240624043034_AuditableAsset.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AssetManagement.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuditableAsset : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "Created",
+                table: "Categories",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastModified",
+                table: "Categories",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastModifiedBy",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "Created",
+                table: "Assets",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Assets",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastModified",
+                table: "Assets",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastModifiedBy",
+                table: "Assets",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "LastModified",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedBy",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Assets");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Assets");
+
+            migrationBuilder.DropColumn(
+                name: "LastModified",
+                table: "Assets");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedBy",
+                table: "Assets");
+        }
+    }
+}
