89995a9ed9ccd41b70d177791b15a756bd0fbe07
nguyentobinh <rookiedev06@toanleh.onmicrosoft.com>
2024-06-24 11:44:25 +0700

    feat: add GetAssetsListWithPagination

---
+++ src/Application/Assets/Queries/GetAssetsWithPagination/AssetBriefDto.cs
@@ -0,0 +1,24 @@
+﻿using AssetManagement.Domain.Entities;
+
+namespace AssetManagement.Application.Assets.Queries.GetAssetsWithPagination;
+
+public class AssetBriefDto
+{
+    public int Id { get; init; }
+    
+    public string Code { get; init; } = null!;
+    
+    public string Name { get; init; } = null!;
+    
+    public string CategoryName { get; init; } = null!;
+    
+    public string AssetStatusName { get; init; } = null!;
+    
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Asset, AssetBriefDto>();
+        }
+    }
+}
