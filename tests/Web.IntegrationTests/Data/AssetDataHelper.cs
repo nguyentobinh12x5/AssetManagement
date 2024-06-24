8e4746d8c4bb4343c20ede92edd55c3719904ab6
Tam Nguyen Chi <rookiedev05@toanleh.onmicrosoft.com>
2024-06-24 21:56:01 +0700

    test: add test integration for get asset by id

---
+++ tests/Web.IntegrationTests/Data/AssetDataHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AssetManagement.Domain.Entities;
+using AssetManagement.Infrastructure.Data;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Web.IntegrationTests.Helpers;
+
+namespace Web.IntegrationTests.Data
+{
+    public static class AssetDataHelper
+    {
+        private static readonly List<Category> Categories = new()
+        {
+            new Category { Id = 1, Name = "Laptop", Code = "LA001" }
+        };
+
+        private static readonly List<AssetStatus> AssetStatuses = new()
+        {
+            new AssetStatus { Id = 1, Name = "Available" }
+        };
+
+        private static readonly List<Asset> Assets = new()
+        {
+            new Asset
+            {
+                Id = 1,
+                Code = "LA100001",
+                Name = "Laptop Probook 450 G1",
+                Location = "HCM",
+                Specification = "Core i5, 8GB RAM, 750 GB",
+                InstalledDate = new DateTime(2021, 1, 16),
+                Category = Categories[0],
+                AssetStatus = AssetStatuses[0]
+            }
+        };
+
+        public static async Task CreateSampleData(TestWebApplicationFactory<Program> factory)
+        {
+            using (var scope = factory.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetService<ApplicationDbContext>();
+                if (db != null)
+                {
+                    db.Categories.AddRange(Categories);
+                    db.AssetStatuses.AddRange(AssetStatuses);
+                    db.Assets.AddRange(Assets);
+                    await db.SaveChangesAsync();
+                }
+            }
+        }
+
+    }
+}
---
+++ tests/Web.IntegrationTests/Endpoints/AssetsTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+using AssetManagement.Application.Assets.Queries.GetDetailedAssets;
+
+using Web.IntegrationTests.Data;
+using Web.IntegrationTests.Extensions;
+using Web.IntegrationTests.Helpers;
+
+using Xunit;
+
+using Assert = Xunit.Assert;
+
+namespace Web.IntegrationTests.Endpoints
+{
+    [Collection("Sequential")]
+    public class AssetsTests : IClassFixture<TestWebApplicationFactory<Program>>
+    {
+        private readonly TestWebApplicationFactory<Program> _factory;
+        private readonly HttpClient _httpClient;
+
+        public AssetsTests(TestWebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _httpClient = _factory.GetApplicationHttpClient();
+        }
+
+        [Fact]
+        public async Task GetAsset_ValidId_ReturnsAssetDto()
+        {
+            // Arrange
+            await AssetDataHelper.CreateSampleData(_factory);
+            var assetId = 1;
+
+            // Act
+            var assetDto = await _httpClient.GetFromJsonAsync<AssetDto>($"/Assets/{assetId}");
+
+            // Assert
+            Assert.NotNull(assetDto);
+            Assert.Equal(assetId, assetDto.Id);
+        }
+
+        [Fact]
+        public async Task GetAsset_InvalidId_ReturnsNotFound()
+        {
+            // Arrange
+            var invalidAssetId = 999;
+
+            // Act
+            var response = await _httpClient.GetAsync($"/Assets/{invalidAssetId}");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
