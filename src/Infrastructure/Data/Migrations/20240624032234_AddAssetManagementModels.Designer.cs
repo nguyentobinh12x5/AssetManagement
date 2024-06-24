8057c886ecd22625b641f96d1aef06fc07a3b114
Kien Nguyen <rookiedev02@toanleh.onmicrosoft.com>
2024-06-24 10:30:06 +0700

    feat: Add Asset and Category models

--- src/Infrastructure/Data/Migrations/20240624032234_AddAssetManagementModels.Designer.cs
+++ src/Infrastructure/Data/Migrations/20240624032234_AddAssetManagementModels.Designer.cs
@@ -4,6 +4,7 @@ using AssetManagement.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AssetManagement.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240624032234_AddAssetManagementModels")]
+    partial class AddAssetManagementModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
