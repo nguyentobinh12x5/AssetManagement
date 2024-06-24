db2a8b449a77f5a4fc2aadf3f4242788e3fa62fe
nguyentobinh <rookiedev06@toanleh.onmicrosoft.com>
2024-06-24 11:44:25 +0700

    feat: add GetAssetsListWithPagination

---
+++ src/Application/Assets/Queries/GetAssetsWithPagination/GetAssetsWithPagination.cs
@@ -0,0 +1,35 @@
+﻿using AssetManagement.Application.Common.Interfaces;
+using AssetManagement.Application.Common.Mappings;
+using AssetManagement.Application.Common.Models;
+using AssetManagement.Application.TodoItems.Queries.GetTodoItemsWithPagination;
+using AssetManagement.Domain.Constants;
+
+namespace AssetManagement.Application.Assets.Queries.GetAssetsWithPagination;
+
+public class GetAssetsWithPaginationQuery : IRequest<PaginatedList<AssetBriefDto>>
+{
+    public int PageNumber { get; init; } = AppPagingConstants.DefaultPageNumber;
+    public int PageSize { get; init; } = AppPagingConstants.DefaultPageSize;
+    public required string SortColumnName { get; init; }
+    public required string SortColumnDirection { get; init; } = AppPagingConstants.DefaultSortDirection;
+}
+
+public class GetAssetsWithPaginationQueryHandler : IRequestHandler<GetAssetsWithPaginationQuery, PaginatedList<AssetBriefDto>>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+    
+    public GetAssetsWithPaginationQueryHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+    public async Task<PaginatedList<AssetBriefDto>> Handle(GetAssetsWithPaginationQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Assets
+            //.OrderBy(x => x.Title)
+            .OrderByDynamic(request.SortColumnName, request.SortColumnDirection)
+            .ProjectTo<AssetBriefDto>(_mapper.ConfigurationProvider)
+            .PaginatedListAsync(request.PageNumber, request.PageSize);
+    }
+}
---
+++ src/Web/Endpoints/Assets.cs
@@ -0,0 +1,20 @@
+﻿using AssetManagement.Application.Assets.Queries.GetAssetsWithPagination;
+using AssetManagement.Application.Common.Models;
+
+namespace AssetManagement.Web.Endpoints;
+
+public class Assets : EndpointGroupBase
+{
+    public override void Map(WebApplication app)
+    {
+        app.MapGroup(this)
+            .AllowAnonymous()
+            .MapGet(GetAssetList);
+    }
+
+    public Task<PaginatedList<AssetBriefDto>> GetAssetList(ISender sender, [AsParameters] GetAssetsWithPaginationQuery query)
+    {
+        return sender.Send(query);
+    }
+    
+}
