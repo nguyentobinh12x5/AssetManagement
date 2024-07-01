b904bf8d490a1dd0018dff687d0245f05cf37c7e
Phuong Nguyen <rookiedev03@toanleh.onmicrosoft.com>
2024-07-01 22:39:29 +0700

    feat(asset): add delete asset handler

---
+++ src/Application/Assets/Commands/Delete/DeleteAsset.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AssetManagement.Application.Common.Interfaces;
+using AssetManagement.Application.Common.Security;
+
+namespace AssetManagement.Application.Assets.Commands.Delete;
+
+[Authorize]
+public record DeleteAssetCommand(int id): IRequest;
+
+[Authorize]
+public class DeleteAssetCommandHandler : IRequestHandler<DeleteAssetCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public DeleteAssetCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(DeleteAssetCommand request, CancellationToken cancellationToken)
+    {
+        var asset = await _context.Assets.FindAsync(request.id);
+
+        Guard.Against.NotFound(request.id, asset);
+
+        _context.Assets.Remove(asset);
+        
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
+
