2b5a4bd3608e3fc1b58584c285b176df66c80145
nguyentobinh <rookiedev06@toanleh.onmicrosoft.com>
2024-07-05 10:42:04 +0700

    fix: update asset state after remove assigment

--- src/Application/Assignments/Commands/Delete/DeleteAssignment.cs
+++ src/Application/Assignments/Commands/Delete/DeleteAssignment.cs
@@ -11,7 +11,7 @@ using AssetManagement.Application.Common.Security;
 namespace AssetManagement.Application.Assignments.Commands.Delete;
 
 [Authorize]
-public record DeleteAssignmentCommand(int id) : IRequest;
+public record DeleteAssignmentCommand(int Id) : IRequest;
 
 [Authorize]
 public class DeleteAssignmentCommandHandler : IRequestHandler<DeleteAssignmentCommand>
@@ -25,11 +25,23 @@ public class DeleteAssignmentCommandHandler : IRequestHandler<DeleteAssignmentCo
 
     public async Task Handle(DeleteAssignmentCommand request, CancellationToken cancellationToken)
     {
-        var assignment = await _context.Assignments.FindAsync(request.id);
+        var assignment = await _context.Assignments
+            .Include(a => a.Asset)
+            .FirstOrDefaultAsync(a => a.Id == request.Id, cancellationToken);
 
-        Guard.Against.NotFound(request.id, assignment);
+        Guard.Against.NotFound(request.Id, assignment);
 
         _context.Assignments.Remove(assignment);
+        
+        var asset = await _context.Assets.FirstOrDefaultAsync(a => a.Id == assignment.Asset.Id, cancellationToken);
+        
+        Guard.Against.NotFound(assignment.Asset.Id, asset);
+        
+        var state = await _context.AssetStatuses.FirstOrDefaultAsync(e => e.Name == "Available", cancellationToken);
+        
+        Guard.Against.NotFound("Available", state);
+        
+        asset.AssetStatus = state;
 
         await _context.SaveChangesAsync(cancellationToken);
     }
