44d96c628fa00223488d36552a02a60c676992e6
Phuong Nguyen <rookiedev03@toanleh.onmicrosoft.com>
2024-07-09 01:00:52 +0700

    feat(returning): add view for returning list

--- src/Web/client-app/src/features/returning/list/index.tsx
+++ src/Web/client-app/src/features/returning/list/index.tsx
@@ -1,5 +1,54 @@
-import React from "react";
+import { Col, Row } from "react-bootstrap";
+import SearchBox from "../../../components/SearchBox/SearchBox";
+import "./ReturningList.scss";
+import ReturningTable from "./ReturingTable";
+import useReturningList from "./useReturningList";
+import FilterByState from "../components/FilterByState";
+import FilterByReturnedDate from "../components/FilterByReturnedDate";
+
 const ReturningList = () => {
-  return <div>Returining Request List</div>;
+  const {
+    hasSortColumn,
+    searchTerm,
+    handleSort,
+    handlePaging,
+    handleSearch,
+    returnings,
+    sortColumnDirection,
+  } = useReturningList();
+
+  return (
+    <div className="asset-list offset-1">
+      <p className="title">Request list</p>
+
+      <Row className="mb-3">
+        <Col md={3}>
+          <FilterByState />
+        </Col>
+        <Col md={3}>
+          <FilterByReturnedDate />
+        </Col>
+        <Col md={{ span: 3, offset: 1 }} className="ml-auto">
+          <SearchBox
+            defaultValue={searchTerm}
+            handleFilterBySearchTerm={handleSearch}
+          />
+        </Col>
+      </Row>
+
+      <ReturningTable
+        returnings={returnings}
+        searchTerm={searchTerm}
+        sortState={{
+          name: hasSortColumn.sortColumn,
+          orderBy: hasSortColumn.sortOrder,
+        }}
+        sortColumnDirection={sortColumnDirection}
+        handleSort={handleSort}
+        handlePaging={handlePaging}
+      />
+    </div>
+  );
 };
+
 export default ReturningList;
---
+++ src/Web/client-app/src/features/returning/list/useReturningList.ts
@@ -0,0 +1,86 @@
+import { useEffect, useCallback } from 'react';
+import { useAppDispatch, useAppState } from '../../../redux/redux-hooks';
+import useAppPaging from '../../../hooks/paging/useAppPaging';
+import useAppSort from '../../../hooks/paging/useAppSort';
+import { IReturningQuery } from '../interfaces/Common/IReturningQuery';
+import { DEFAULT_MANAGE_RETURNING_SORT_COLUMN } from '../constants/returning-sort';
+import { getReturnings, setReturningQuery } from '../reducers/returning-slice';
+
+
+
+const useReturningList = () => {
+    const dispatch = useAppDispatch();
+    const { returnings, returningQuery, isDataFetched } = useAppState(
+        (state) => state.returnings
+    );
+
+    const updateQuery = useCallback(
+        (newQuery: Partial<IReturningQuery>) => {
+            dispatch(setReturningQuery({ ...returningQuery, ...newQuery }));
+        },
+        [dispatch, returningQuery]
+    );
+
+    const updateMainSortState = useCallback(
+        (sortColumnName: string, sortColumnDirection: string) => {
+            updateQuery({ sortColumnName, sortColumnDirection });
+        },
+        [updateQuery]
+    );
+
+    const updateMainPagingState = useCallback(
+        (page: number) => {
+            updateQuery({ pageNumber: page });
+        },
+        [updateQuery]
+    );
+
+    const handleFilterByState = useCallback(
+        (status: string[]) => {
+            updateQuery({ pageNumber: 1, state: status });
+        },
+        [updateQuery]
+    );
+
+    const handleSearch = useCallback(
+        (searchTerm: string) => {
+            updateQuery({ pageNumber: 1, searchTerm: searchTerm.trim() });
+        },
+        [updateQuery]
+    );
+
+    const handleFilterByReturnedDate = useCallback(
+        (returnedDate: string) => {
+            updateQuery({ pageNumber: 1, returnedDate });
+        },
+        [updateQuery]
+    );
+
+    // Fetch Data
+    useEffect(() => {
+        if (!isDataFetched) {
+            dispatch(getReturnings(returningQuery));
+        }
+    }, [dispatch, returningQuery, isDataFetched]);
+
+    // Pagination and Sorting hooks
+    const { hasSortColumn, handleSort } = useAppSort(
+        DEFAULT_MANAGE_RETURNING_SORT_COLUMN,
+        updateMainSortState
+    );
+    const { handlePaging } = useAppPaging(updateMainPagingState);
+
+    return {
+        hasSortColumn,
+        returnings,
+        searchTerm: returningQuery.searchTerm,
+        handleSort,
+        handlePaging,
+        handleFilterByState,
+        handleSearch,
+        handleFilterByReturnedDate,
+        sortColumnDirection: returningQuery.sortColumnDirection,
+    };
+};
+
+export default useReturningList;
