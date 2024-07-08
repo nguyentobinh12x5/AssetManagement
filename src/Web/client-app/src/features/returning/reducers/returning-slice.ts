810a11c146415ca237e1e43c2102b56460ae0764
Phuong Nguyen <rookiedev03@toanleh.onmicrosoft.com>
2024-07-09 00:55:40 +0700

    feat(returning): add get list returning slice

--- src/Web/client-app/src/features/returning/reducers/returning-slice.ts
+++ src/Web/client-app/src/features/returning/reducers/returning-slice.ts
@@ -1,34 +1,70 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { IReturningQuery } from '../interfaces/Common/IReturningQuery';
+import { APP_DEFAULT_PAGE_SIZE, ASCENDING } from '../../../constants/paging';
+import { DEFAULT_MANAGE_RETURNING_SORT_COLUMN } from '../constants/returning-sort';
+import { IPagedModel } from '../../../interfaces/IPagedModel';
+import { IBriefReturning } from '../interfaces/IBriefReturning';
 
+const defaultReturningQuery: IReturningQuery = {
+    pageNumber: 1,
+    pageSize: APP_DEFAULT_PAGE_SIZE,
+    sortColumnDirection: ASCENDING,
+    sortColumnName: DEFAULT_MANAGE_RETURNING_SORT_COLUMN,
+    state: ['All'],
+    returnedDate: '',
+    searchTerm: '',
+};
 interface ReturningState {
-  returnings: any[];
-  isLoading: boolean;
-  error?: string | null;
+    returnings: IPagedModel<IBriefReturning>;
+    isLoading: boolean;
+    error?: string | null;
+    returningQuery: IReturningQuery;
+    isDataFetched: boolean;
+    states: string[];
 }
 
 const initialState: ReturningState = {
-  returnings: [],
-  isLoading: false,
+    returnings: {
+        items: [],
+        pageNumber: 1,
+        totalPages: 1,
+        totalCount: 0,
+        hasPreviousPage: false,
+        hasNextpage: false,
+    },
+    states: ['All'],
+    isLoading: false,
+    returningQuery: defaultReturningQuery,
+    isDataFetched: false,
+    error: null,
 };
 const ReturningSlice = createSlice({
-  initialState,
-  name: 'returning',
-  reducers: {
-    getReturnings: (state: ReturningState) => ({
-      ...state,
-      isLoading: true,
-    }),
-
+    initialState,
+    name: 'returning',
+    reducers: {
+        setReturningQuery: (state: ReturningState, action: PayloadAction<IReturningQuery>) => {
+            return {
+                ...state,
+                returningQuery: action.payload,
+                isDataFetched: false,
+            };
+        },
+        getReturnings: (state: ReturningState, action: PayloadAction<IReturningQuery>) => ({
+            ...state,
+            isLoading: true,
+        }),
+        
     // Success handles
     getReturningsSuccess: (
       state: ReturningState,
-      action: PayloadAction<any>
+        action: PayloadAction<any>
     ) => ({
-      ...state,
-      isLoading: false,
-      returnings: action.payload,
-    }),
-
+        ...state,
+        isLoading: false,
+        isDataFetched: true,
+        returnings: action.payload,
+        }),
+        
     // Failure handles
     getReturningsFailure: (
       state: ReturningState,
@@ -37,11 +73,12 @@ const ReturningSlice = createSlice({
       ...state,
       isLoading: false,
       error: action.payload,
-    }),
+        }),
+        resetReturningSlice: () => initialState,
   },
 });
 
-export const { getReturnings, getReturningsSuccess, getReturningsFailure } =
+export const { getReturnings, setReturningQuery,getReturningsSuccess, getReturningsFailure } =
   ReturningSlice.actions;
 
 export default ReturningSlice.reducer;
