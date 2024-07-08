6cc66f66e2be7ae2c0ebc8080a8d3aa8cdaa559f
Tam Nguyen Chi <rookiedev05@toanleh.onmicrosoft.com>
2024-07-08 16:04:52 +0700

    feat: add API create request returning asset

---
+++ src/Application/ReturningRequests/Commands/Create/CreateRequestReturningAsset.cs
@@ -0,0 +1,55 @@
+﻿using AssetManagement.Application.Common.Interfaces;
+using AssetManagement.Domain.Entities;
+using AssetManagement.Domain.Enums;
+
+using FluentValidation;
+
+using MediatR;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using AssetManagement.Application.Common.Security;
+
+namespace AssetManagement.Application.ReturningRequests.Commands.Create;
+[Authorize]
+public record CreateRequestReturningAssetCommand : IRequest<int>
+{
+    public int AssignmentId { get; init; }
+}
+[Authorize]
+
+public class CreateRequestReturningAssetCommandHandler : IRequestHandler<CreateRequestReturningAssetCommand, int>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IUser _currentUser;
+
+    public CreateRequestReturningAssetCommandHandler(IApplicationDbContext context, IUser currentUser)
+    {
+        _context = context;
+        _currentUser = currentUser;
+    }
+
+    public async Task<int> Handle(CreateRequestReturningAssetCommand request, CancellationToken cancellationToken)
+    {
+        var assignment = await _context.Assignments.FindAsync(request.AssignmentId);
+        Guard.Against.NotFound(request.AssignmentId, assignment);
+
+        Guard.Against.NullOrEmpty(_currentUser.UserName);
+
+        var returningRequest = new ReturningRequest
+        {
+            AssignmentId = request.AssignmentId,
+            RequestedBy = _currentUser.UserName,
+            State = ReturningRequestState.WaitingForReturning,
+            IsDelete = false,
+            Assignment = assignment!
+        };
+
+        _context.ReturningRequests.Add(returningRequest);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return returningRequest.Id;
+    }
+}
--- src/Web/Endpoints/ReturningRequests.cs
+++ src/Web/Endpoints/ReturningRequests.cs
@@ -1,3 +1,10 @@
+using AssetManagement.Application.ReturningRequests.Commands.Create;
+using AssetManagement.Application.Common.Models;
+using AssetManagement.Application.Common.Interfaces;
+using AssetManagement.Domain.Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace AssetManagement.Web.Endpoints;
 
 public class ReturningRequests : EndpointGroupBase
@@ -5,6 +12,12 @@ public class ReturningRequests : EndpointGroupBase
     public override void Map(WebApplication app)
     {
         app.MapGroup(this)
-            .AllowAnonymous();
+            .AllowAnonymous()
+            .MapPost(CreateReturningRequest, "Create");
+    }
+
+    public Task<int> CreateReturningRequest(ISender sender, CreateRequestReturningAssetCommand command)
+    {
+        return sender.Send(command);
     }
 }
