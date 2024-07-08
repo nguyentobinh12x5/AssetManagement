96754caefbbc90bd90530823a861f9f714510ec4
Phuong Nguyen <rookiedev03@toanleh.onmicrosoft.com>
2024-07-09 00:58:06 +0700

    feat(returning): add returning brief interface

--- src/Web/client-app/src/features/returning/interfaces/IBriefReturning.ts
+++ src/Web/client-app/src/features/returning/interfaces/IBriefReturning.ts
@@ -1,1 +1,12 @@
-export interface IBriefReturning {}
+import { ReturningState } from "../constants/returning-state";
+
+export interface IBriefReturning {
+    id: string;
+    assetCode: string;
+    assetName: string;
+    requestedBy: string;
+    assignedDate: string;
+    acceptedBy: string;
+    returnedDate: string;
+    state: ReturningState;
+}
